81aa094ce3e48552d42e57ba962df3fff0b21d65
Gaurav3170 <gauravcs0062@gmail.com>
2020-05-28 16:58:45 +0530

    Updates

---
+++ precache-manifest.a8ebe232080653d56358df6a182cb431.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fe0f810822316b887a1c4b9806c1de6",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "215e5ea36199de8df5b8",
+    "url": "/deploy-react/static/css/main.c9510dee.chunk.css"
+  },
+  {
+    "revision": "e176a7a1bedeee1a1460",
+    "url": "/deploy-react/static/js/2.f1028995.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/deploy-react/static/js/2.f1028995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "215e5ea36199de8df5b8",
+    "url": "/deploy-react/static/js/main.6ef67d89.chunk.js"
+  },
+  {
+    "revision": "ec9443a65c8540c1adcd",
+    "url": "/deploy-react/static/js/runtime-main.69db8624.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/deploy-react/static/media/image.d7265326.png"
+  }
+]);
